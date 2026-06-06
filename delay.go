dc3358d4193cba10e50d105c295cd965b9cf2dd8
Momchil Atanasov <momchil.dev@gmail.com>
2026-06-06 22:34:02 +0300

    Rework DelayNode type

--- delay.go
+++ delay.go
@@ -1,22 +1,23 @@
 package wasmal
 
 // DelayNode as described here:
-// https://developer.mozilla.org/en-US/docs/Web/API/DelayNode
+// https://www.w3.org/TR/webaudio-1.1/#DelayNode
 type DelayNode interface {
 	AudioNode
 
+	// DelayTime returns the delay time parameter of this DelayNode.
 	DelayTime() AudioParam
 }
 
-var _ DelayNode = goDelayNode{}
+var _ DelayNode = (*goDelayNode)(nil)
 
 type goDelayNode struct {
 	goAudioNode
 }
 
-func (g goDelayNode) DelayTime() AudioParam {
+func (g *goDelayNode) DelayTime() AudioParam {
 	jsValue := g.jsValue.Get("delayTime")
-	return goAudioParam{
+	return &goAudioParam{
 		goObject: goObject{
 			jsValue: jsValue,
 		},
