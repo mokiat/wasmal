083d0cc0940388919217664ed7b751f7a87feb9b
Momchil Atanasov <momchil.dev@gmail.com>
2026-06-06 20:39:38 +0300

    Add Float32Array type

---
+++ array.go
@@ -0,0 +1,24 @@
+package wasmal
+
+// Float32Array as described here:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Float32Array
+type Float32Array interface {
+	Get(index int) float32
+	Set(index int, value float32)
+}
+
+var _ Float32Array = (*goFloat32Array)(nil)
+
+type goFloat32Array struct {
+	goObject
+}
+
+// Get returns the value at the given index.
+func (g goFloat32Array) Get(index int) float32 {
+	return float32(g.jsValue.Index(index).Float())
+}
+
+// Set sets the value at the given index.
+func (g goFloat32Array) Set(index int, value float32) {
+	g.jsValue.SetIndex(index, value)
+}
