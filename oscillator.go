04ef7c931179e16d85af7a4763147714bf026228
Momchil Atanasov <momchil.dev@gmail.com>
2026-06-06 22:23:23 +0300

    Rework OscillatorNode type

--- oscillator.go
+++ oscillator.go
@@ -1,18 +1,25 @@
 package wasmal
 
 // OscillatorNode as described here:
-// https://developer.mozilla.org/en-US/docs/Web/API/OscillatorNode
+// https://www.w3.org/TR/webaudio-1.1/#OscillatorNode
 type OscillatorNode interface {
 	AudioScheduledSourceNode
 
-	Frequency() AudioParam
-	Detune() AudioParam
+	// Type returns the type of the oscillator. The default value is "sine".
 	Type() OscillatorType
+
+	// SetType sets the type of the oscillator.
 	SetType(oType OscillatorType)
+
+	// Frequency returns an AudioParam representing the frequency of the oscillator.
+	Frequency() AudioParam
+
+	// Detune returns an AudioParam representing the detuning of the oscillator in cents.
+	Detune() AudioParam
 }
 
 // OscillatorType as described here:
-// https://developer.mozilla.org/en-US/docs/Web/API/OscillatorNode/type
+// https://www.w3.org/TR/webaudio-1.1/#enumdef-oscillatortype
 type OscillatorType string
 
 const (
@@ -23,34 +30,34 @@ const (
 	OscillatorTypeCustom   OscillatorType = "custom"
 )
 
-var _ OscillatorNode = goOscillatorNode{}
+var _ OscillatorNode = (*goOscillatorNode)(nil)
 
 type goOscillatorNode struct {
 	goAudioScheduledSourceNode
 }
 
-func (g goOscillatorNode) Frequency() AudioParam {
-	jsValue := g.jsValue.Get("frequency")
-	return goAudioParam{
-		goObject: goObject{
-			jsValue: jsValue,
-		},
-	}
-}
-
-func (g goOscillatorNode) Detune() AudioParam {
-	jsValue := g.jsValue.Get("detune")
-	return goAudioParam{
-		goObject: goObject{
-			jsValue: jsValue,
-		},
-	}
-}
-
-func (g goOscillatorNode) Type() OscillatorType {
+func (g *goOscillatorNode) Type() OscillatorType {
 	return OscillatorType(g.jsValue.Get("type").String())
 }
 
-func (g goOscillatorNode) SetType(oType OscillatorType) {
+func (g *goOscillatorNode) SetType(oType OscillatorType) {
 	g.jsValue.Set("type", string(oType))
+}
+
+func (g *goOscillatorNode) Frequency() AudioParam {
+	jsValue := g.jsValue.Get("frequency")
+	return &goAudioParam{
+		goObject: goObject{
+			jsValue: jsValue,
+		},
+	}
+}
+
+func (g *goOscillatorNode) Detune() AudioParam {
+	jsValue := g.jsValue.Get("detune")
+	return &goAudioParam{
+		goObject: goObject{
+			jsValue: jsValue,
+		},
+	}
 }
